99d2dbdc0aee2f2ae7de29a7c83005ba85abe8e6
emomhossan21 <135006402+emomhossan21@users.noreply.github.com>
2025-10-24 18:37:15 +0300

    Update best-ok.py

--- best-ok.py
+++ best-ok.py
@@ -57,7 +57,7 @@ cokbrut=[]
 ses=requests.Session()
 princp=[]
 try:
- prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=10000000&country=all&ssl=all&anonymity=all').text
+ prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
  open('.prox.txt','w').write(prox)
 except Exception as e:
  print('')
